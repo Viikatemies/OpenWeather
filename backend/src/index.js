0a88450bf1e3ba287742234b892efe741ce4c611
JaniRinne <janrinne@gmail.com>
2019-11-05 20:23:42 +0200

    Update City and API key. Change to from current weather to forecast. Add repository to package

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,9 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
+const appId = process.env.APPID || '8fc5f2fd5c807726254474e1ca5a35f5';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
+const targetCity = process.env.TARGET_CITY || "Tampere,fi";
 
 const port = process.env.PORT || 9000;
 
@@ -16,7 +16,7 @@ const app = new Koa();
 app.use(cors());
 
 const fetchWeather = async () => {
-  const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
+  const endpoint = `${mapURI}/forecast/hourly?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
 
   return response ? response.json() : {}
